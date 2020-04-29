aade805c5814cbc3eea196c7d33a2daed02bb126
devpool <fabio_jesus@live.com>
2020-04-29 09:11:44 +0100

    Updated structure

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,49 @@
 ﻿using Arrays;
+using Pascaleira.Fase1;
 using System;
 
 namespace ConsoleApp1
 {
+
+    public static class ExtensionMethods
+    {
+        public static bool IsBetweenAndDifferentFromAndDiviseableBy(this long i, long min, long max, long different, long divisor)
+        {
+            return i > min && i < max && i != different && i % divisor == 0;
+        }
+    }
+
+
     class Program
     {
+        
+
         static void Main(string[] args)
         {
 
-            //CoisasECenas.MinNumber();
-
-            Console.WriteLine(CoisasECenas.Average(null));
+            long i = 0;
+            //var isBetween = i.IsBetween(10, 20);
+            var firstIsBetween = i.IsBetweenAndDifferentFromAndDiviseableBy(10, 20, 8, 24);
+            var secondIsBetween = i.IsBetweenAndDifferentFromAndDiviseableBy(18, 50, 31, 98);
+            
+            if(firstIsBetween)
+            {
+                char i = 'a';
+                char.Is
+                i = 3;
+            }
+            else
+            {
+                i = 4;
+            }
+            if (secondIsBetween)
+            {
+                i = 3;
+            }
+            else
+            {
+                i = 4;
+            }
 
         }
     }
---
+++ Pascaleira/First/Base.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Pascaleira.Fase1
+{
+    public static class Pascalator
+    {
+        /// <summary>
+        /// Sums two numbers
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>The sum of two numbers</returns>
+        public static double Sum(double number1, double number2)
+        {
+            double sum = 0;
+            sum = number1 + number2;
+            return sum;
+        }
+        /// <summary>
+        /// Subtracts based on the two numbers inserted
+        /// </summary>
+        /// <param name="number1">First number inserted</param>
+        /// <param name="number2">Second number inserted</param>
+        /// <returns>The substraction of two numbers</returns>
+
+        public static double Subtract(double number1, double number2)
+        {
+            double subtraction = 0;
+            subtraction = number1 - number2;
+            return subtraction;
+        }
+        /// <summary>
+        /// Divides two numbers
+        /// </summary>
+        /// <param name="number1">The first number</param>
+        /// <param name="number2">The second number</param>
+        /// <returns>The division of two numbers</returns>
+        public static double Divide(double number1, double number2)
+        {
+            double division = 0;
+            if (number2 == 0)
+            {
+                return 0;
+            }
+            division = number1 / number2;
+            return division;
+
+        }
+        /// <summary>
+        /// Multiplication of two given numbers
+        /// </summary>
+        /// <param name="number1">First number</param>
+        /// <param name="number2">Second number</param>
+        /// <returns>Multiplication of said numbers</returns>
+        public static double Multiplication(double number1, double number2)
+        {
+            double multiplication = number1 * number2;
+            return multiplication;
+        }
+
+        /// <summary>f
+        /// Remainder of division between two numbers
+        /// </summary>
+        /// <param name="number1">First number</param>
+        /// <param name="number2">Second number</param>
+        /// <returns>Remainder of two numbers</returns>
+        public static double Remainder(double number1, double number2)
+        {
+            double remainder = number1 % number2;
+            return remainder;
+
+        }
+
+        /// <summary>
+        /// Presents the min number of array
+        /// </summary>
+        /// <param name="numbers">Numbers array</param>
+        /// <returns>Returns the lesser value of the array</returns> 
+        public static int Min(int[] numbers)
+        {
+            int num = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (num > numbers[i])
+                {
+                    num = numbers[i];
+                }
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// Presents the greatest number of array
+        /// </summary>
+        /// <param name="numbers">Numbers array</param>
+        /// <returns>Returns the greatest value of the array</returns> 
+        public static int Max(int[] numbers)
+        {
+            int num = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (num < numbers[i])
+                {
+                    num = numbers[i];
+                }
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// Determines number one to the power of number two
+        /// </summary
+        /// <param name="number1">The base number</param>
+        /// <param name="number2">The exponent number</param>
+        /// <returns>Powered number</returns>
+        public static double Power(double number1, double number2)
+        {
+            return Math.Pow(number1, number2);
+        }
+
+        /// <summary>
+        /// Determine the average of an array
+        /// </summary>
+        /// <param name="numbers">The array of numbers</param>
+        /// <returns>The average of all the numbers in the array</returns>
+        public static int Average(int[] numbers)
+        {
+            int average = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+                average += numbers[i];
+
+            return (average / numbers.Length);
+        }
+    }
+
+
+}
